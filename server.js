890e421624e85691a499b175aed472e51914a65b
Ali BELHACENE <abelhacene@wemanity.com>
2022-02-09 09:43:23 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-'use strict';
-
 const express = require('express');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
 
-// App
+const PORT = 8080;
+
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
